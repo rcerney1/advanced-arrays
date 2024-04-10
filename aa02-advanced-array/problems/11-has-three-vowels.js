5275c374972986931d4b63b6de290a302deba370
rcerney <ryanccerney@gmail.com>
2024-04-10 15:20:32 -0700

    11

--- aa02-advanced-array/problems/11-has-three-vowels.js
+++ aa02-advanced-array/problems/11-has-three-vowels.js
@@ -17,8 +17,31 @@ console.log(hasThreeVowels('go home'));         //  false
 */
 
 let hasThreeVowels = function(string) {
-    // Your code here 
+    // Your code here
+    let vowels = 'aeiouAEIOU';
+    let count = 0;
+    let array = string.split('');
+    let each = array.filter((letter) => {
+       return (vowels.indexOf(letter) !== -1);
+    })
+    let obj = {};
+    for (let i = 0; i < each.length; i++) {
+        if(obj[each[i]] === undefined) {
+            obj[each[i]] = 1;
+            count++;
+        } 
+    }
+
+    if(count >= 3) {
+        return true;
+    } else {return false}
 };
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
 
 // Your code here 
 
